71d1732b122613ba29a77b95fe84c4c9a56f0b1d
Adam Moore <nerdfunk@gmail.com>
2019-01-16 12:44:17 +0900

    update handling

--- src/formic/components/quill.cljs
+++ src/formic/components/quill.cljs
@@ -1,12 +1,13 @@
 (ns formic.components.quill
   (:require
    [formic.util :as u]
+   [formic.components.inputs :as inputs]
    [reagent.core :as r]
    [reagent.impl.component :refer [extract-props]]
    [cljsjs.quill]
    [formic.field :as field]
-   [clojure.string :as s]
-   [goog.object :as gobj]))
+   [goog.object :as gobj]
+   [clojure.string :as str]))
 
 (defn replace-contents [editor value]
   (let [sel (.getSelection editor)]
@@ -26,15 +27,20 @@
    :formats ["bold" "italic" "underline" "list" "indent" "bullet" "link"]})
 
 (defn DEFAULT_SERIALIZER [v]
-  (when (not (string? v)) ;; can be string on initialization
-    (js->clj (gobj/get (:delta v) "ops") :keywordize-keys true)))
+  (if-let [delta (gobj/get (:delta v) "ops")]
+    (js->clj delta :keywordize-keys true)
+    v))
 
 (def not-blank
   {:message "Required"
    :optional true
-   :validate (fn [v] (not-empty (:txt v)))})
+   :validate (fn [v]
+               (if (:txt v)
+                (not (str/blank? (:txt v)))
+                (not-empty v)))})
 
-(defn quill [{:keys [id touched value err classes options]}]
+
+(defn quill [{:keys [id touched value err classes options title]}]
   (let [{:keys [modules formats theme]} options
         element (r/atom nil)
         editor (r/atom nil)
@@ -52,9 +58,10 @@
               ed (js/Quill. @element options)
               reset-ed-fn 
               (fn []
-                (if (string? @value)
-                  (.setText ed @value)
-                  (.setContents ed @value)))]
+                (cond
+                  (string? @value) (.setText ed @value)
+                  (:delta @value) (.setContents ed (:delta @value))
+                  :else (.setContents ed @value)))]
           (reset! current-handler
                   (fn [delta olddelta source]
                     (when (= source "user")
@@ -97,20 +104,16 @@
           (.off ed "editor-change")
           (reset! editor nil)))
       :reagent-render
-      (fn [{:keys [id touched value err options]}]
-        [:div.formic-quill {:class (when @err "error")}
-         [:h5.formic-input-title
-          {:class (:title classes)}
-          (u/format-kw id)]
+      (fn [{:keys [id touched value err options title] :as f}]
+        [inputs/common-wrapper
+         f
          [:div.formic-quill-editor-wrapper
-          [:div 
-           {:ref (fn [el] (reset! element el))}]]
-         [:input {:type "hidden" :value (prn-str (DEFAULT_SERIALIZER @value))}]
-         (when @err
-           [:h3.error @err])])})))
+          [:div.formic-quill-editor-root {:ref (fn [el] (reset! element el))}]
+          [:input {:type "hidden" :value (prn-str (DEFAULT_SERIALIZER @value))}]]]
+       )})))
 
 (field/register-component
  :formic-quill
  {:component quill
-  :parser clj->js
+  :parser (fn [v] (clj->js v))
   :serializer DEFAULT_SERIALIZER})
